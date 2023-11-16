5e255b56341d6013eb7b420dec350b8dfb10e027
Henry Le <lehieu2077@gmail.com>
2023-11-17 05:15:37 +0700

    feat: add debouce hook

--- fe/src/hooks/index.ts
+++ fe/src/hooks/index.ts
@@ -1,1 +1,2 @@
 export { default as useSessionStore } from './useSessionStore';
+export { default as useDebounce } from './useDebounce';
---
+++ fe/src/hooks/useDebounce.ts
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+
+const useDebounce = (
+    value: string,
+    delay: number // in ms
+) => {
+    const [debouncedValue, setDebouncedValue] = useState(value);
+
+    useEffect(() => {
+        const handler = setTimeout(() => {
+            setDebouncedValue(value);
+        }, delay);
+
+        return () => {
+            clearTimeout(handler);
+        };
+    }, [value, delay]);
+
+    return debouncedValue;
+}
+
+export default useDebounce;
