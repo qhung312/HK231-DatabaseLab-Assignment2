c730e144475a0d503a6c269f15640fc50f60a863
Quoc Hung <truongquochung312@gmail.com>
2023-11-23 21:22:43 +0700

    fix: query all employees

--- BE/src/routes/employee.controller.ts
+++ BE/src/routes/employee.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Router } from 'express';
+import _ from 'lodash';
 
 import pool from '../database/database_connection';
 import { CustomResponse } from '../types';
@@ -7,6 +8,30 @@ const employeeController = Router();
 
 employeeController.get('/', async (req: Request, res: CustomResponse) => {
   try {
+    type EmployeeQuery = {
+      queryString: string;
+      value: string;
+    };
+    const queries: EmployeeQuery[] = [];
+    if (req.query.name) {
+      queries.push({
+        queryString: `e_name=$${queries.length + 1}`,
+        value: decodeURIComponent(req.query.name as string)
+      });
+    }
+    if (req.query.id) {
+      queries.push({
+        queryString: `e_id=$${queries.length + 1}`,
+        value: req.query.id as string
+      });
+    }
+    if (req.query.role) {
+      queries.push({
+        queryString: `e_type=$${queries.length + 1}`,
+        value: req.query.role as string
+      });
+    }
+
     const { rows } = await pool.query(
       `
     SELECT
@@ -15,9 +40,9 @@ employeeController.get('/', async (req: Request, res: CustomResponse) => {
       e_type AS "role",
       is_head AS "isHead"
     FROM employee
-    ${req.query.role ? 'WHERE is_head = $1' : ''}
+    ${queries.length > 0 ? `WHERE ${_.join(_.map(queries, 'queryString'), ' AND ')}` : ''}
     `,
-      req.query.role ? [req.query.role] : []
+      _.map(queries, (option) => option.value)
     );
 
     res.composer.ok(rows);
